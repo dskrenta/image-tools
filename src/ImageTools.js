a31e195ca8e2d3498a61cf9ad3f3c65fcbfa363a
dskrenta <dskrenta@gmail.com>
2017-04-05 21:39:57 -0700

    image-tools editor improvements

--- src/ImageTools.js
+++ src/ImageTools.js
@@ -5,12 +5,22 @@ class ImageTools extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      filters: {
+      values: {
         brt: 100,
         sat: 100,
         con: 0
-      }
+      },
+      editSpec: 'brt100-sat100-con0x100',
+      id: 'S5V10IJO9MAS1NJ1'
     };
+  }
+
+  componentDidMount() {
+
+  }
+
+  componentWillUnmount() {
+
   }
 
   render() {
@@ -18,27 +28,40 @@ class ImageTools extends React.Component {
       <div className="image-tools">
         <div className="menu">
           <div className="content-wrap">
-            <button onClick={this.done}>Done</button>
             <form onChange={this.updateFilters}>
               <label>Brightness</label>
-              <input type="range" data-type="brt" value={this.state.filters.brt} min="100" max="300"></input>
+              <input type="range" data-type="brt" value={this.state.values.brt} min="100" max="300"></input>
               <label>Saturation</label>
-              <input type="range" data-type="sat" value={this.state.filters.sat} min="100" max="300"></input>
+              <input type="range" data-type="sat" value={this.state.values.sat} min="100" max="300"></input>
               <label>Contrast</label>
-              <input type="range" data-type="con" value={this.state.filters.con} min="0" max="50"></input>
+              <input type="range" data-type="con" value={this.state.values.con} min="0" max="50"></input>
             </form>
+            <button onClick={this.reset}>Reset</button>
           </div>
+        </div>
+        <div className="crop-container">
+          <img className="preview-image" src={`http://proxy.topixcdn.com/ipicimg/${this.state.id}-${this.state.editSpec}`} alt="preview-image"/>
         </div>
       </div>
     );
   }
 
+  reset = (event) => {
+    this.updateEditSpec();
+  }
+
+  updateEditSpec = (values = {brt: 100, sat: 100, con: 0}) => {
+    const editSpec = `brt${values.brt}-sat${values.sat}-con${values.con}x${100 - values.con}`;
+    this.setState({values: values, editSpec: editSpec});
+  }
+
   updateFilters = (event) => {
     const value = event.target.value;
     const type = event.target.dataset.type;
-    const filters = this.state.filters;
-    filters[type] = value;
-    this.setState({filters});
+    const values = this.state.values;
+    values[type] = value;
+    this.updateEditSpec(values);
+
   }
 }
 
